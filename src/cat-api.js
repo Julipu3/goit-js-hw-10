cf1ef0a85269297a42e8c2515f0a6e9ff4fc7dde
Julipu3 <julia.pim55@gmail.com>
2023-08-27 17:42:32 +0300

    changed

---
+++ src/cat-api.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_KEY = "live_VhtyYnkyxMgfx5Bi5E4pd702JpEuRLc28Ufpscydr16jp6skkL1koymAXPZMTipi";
+const headers = {
+  "x-api-key": API_KEY
+};
+
+export function fetchBreeds() {
+  const BASE_URL = "https://api.thecatapi.com/v1/breeds";
+
+  return axios.get(BASE_URL, { headers })
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.statusText);
+      }
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error.message);
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  const SEARCH_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+
+  return axios.get(SEARCH_URL, { headers })
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.statusText);
+      }
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error.message);
+    });
+}
+
+  
